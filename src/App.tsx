eb6066b8705edb6bf495eb75be3926fd18cc37aa
Omitsusan199411 <59048819+Omitsusan199411@users.noreply.github.com>
2022-06-05 18:15:35 +0900

    chakra-ui実装例 ボタンと背景色を適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import "./styles.css";
+import { theme } from "./theme/theme";
 import { VFC } from "react";
+import { Button, ChakraProvider } from "@chakra-ui/react";
 
 export const App: VFC = () => {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    // rootとなるファイルの最上位の階層にChakraProvidrタグを記述すること
+    <ChakraProvider theme={theme}>
+      <Button colorScheme="teal">ボタン</Button>
+      <p>aaaaaaaaaaa</p>
+    </ChakraProvider>
   );
 };
